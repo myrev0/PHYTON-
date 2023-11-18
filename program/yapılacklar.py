1e9f087e4cfaf480c8ecf08d66e8e6c6208c83d0
myrev0 <150507068+myrev0@users.noreply.github.com>
2023-11-18 10:38:43 +0300

    s

---
+++ program/yapılacklar.py
@@ -0,0 +1,20 @@
+# Sytnax Night Code 
+
+
+print("Yapılacaklar")
+
+print("Yapılacaklar Listemde Şunlar Var")
+
+print("1- Öncelikle Consol Kurumu İçin Main.py")
+
+print("2- Konsol.py İle Login Screen Şifreli Bir Şekilde")
+
+print("3- Daha Sonra Hesap Makinesi")
+
+print("4- Daha Sonra E vektörel amacı ise puan öğrenme puan hesaplama")
+
+print("4- Sayı Tahmin Oyunu. Kütüphane Kullanımı, İf Kullanımı, Ve For Kullanımı Tekrarı için Önemli Bir Oyun Olacak"),
+
+print("5- Yaş Teyit Yapacağım. Yaşını Soracak Eğer 18 Altı İse Bazı Şeyleri Kısıtlayacak")
+
+print("Yapılanların Yanına + İşareti Koyacağım")
