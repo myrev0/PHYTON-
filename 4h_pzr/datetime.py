d5362b5270a28dd1c5be628426a849863f4e8d0d
myrev0 <150507068+myrev0@users.noreply.github.com>
2023-11-19 10:39:58 +0300

    s

---
+++ 4h_pzr/datetime.py
@@ -0,0 +1,18 @@
+# #syntax night code 
+
+# import datetime 
+
+
+# abc = datetime.date.today()
+
+# print("Bu Gün", abc)
+
+
+# abd = datetime.time()
+    
+# print("Şimdi", abd)
+
+
+# zaman = str(datetime.tzinfo())
+
+# print("Şimdi", zaman)
