6c29e0c260b77044cdfd1ed99da31283b252ece1
myrev0 <150507068+myrev0@users.noreply.github.com>
2023-11-25 12:34:00 +0300

    Add files via upload

---
+++ night-galeri/kullanici.py
@@ -0,0 +1,17 @@
+import sistem
+
+def login_screen():
+    print("Kullanıcı Oluşturma Ekranına Hoş Geldiniz.")
+
+    belirle_username = input("Kullanıcı Adı Belirleyiniz.")
+    belirle_password = input("Şifre Belirleyiniz.")
+
+    gir_username = input("Lütfen Kullanıcı Adınızı Giriniz.")
+    gir_password = input("Lütfen Şifre Giriniz.")
+
+    if belirle_username == gir_username and belirle_password == gir_password:
+        print("Tebrikler Başarıyla Girdiniz.")
+        sistem.sistem()
+    else:
+        print("Malesef Giriş Yapamadınız. Şifre Ve ya Kullanıcı Adı Hatalı.")
+        exit()
