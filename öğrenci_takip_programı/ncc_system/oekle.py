48d554bd163c8b63702c872d3c2fde52f798bdf0
myrev0 <150507068+myrev0@users.noreply.github.com>
2023-12-16 11:41:15 +0300

    asd

---
+++ öğrenci_takip_programı/ncc_system/oekle.py
@@ -0,0 +1,8 @@
+def ekle():
+    print("Öğrenci Ekleme Programına Hoş Geldiniz.")
+    class ekle:
+        def __init__(self,ad,soyad,numara):
+            self.ad = ad 
+            self.soyad = soyad 
+            self.numara = numara 
+
